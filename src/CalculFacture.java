b606a37d5f6925046ff6b9213e314d2d6b3497a4
loveyoumaryme <love-mary.victor@outlook.com>
2025-03-01 14:10:03 -0500

    feature: Adding CalculFacture

---
+++ src/CalculFacture.java
@@ -0,0 +1,22 @@
+public class CalculFacture {
+
+    public static int voituresDisponbiles(int nbLouesHP,
+                                          int nbLouesHI,
+                                          int nbLouesHG,
+                                          int nbLouesEP,
+                                          int nbLouesEI,
+                                          int nbLouesEG) {
+
+        int nbRestantesHP = 12;
+        int nbRestantesHI = 10;
+        int nbRestantesHG = 3;
+        int nbRestantesEP = 11;
+        int nbRestantesEI = 9;
+        int nbRestantesEG = 5;
+
+
+
+
+        return 0;
+    }
+}
