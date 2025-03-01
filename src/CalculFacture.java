cc922d4a4ad93758aeff3e76c15828b056d84e10
loveyoumaryme <love-mary.victor@outlook.com>
2025-03-01 16:32:06 -0500

    feature: adding price per day for each type and size of vehicule

--- src/CalculFacture.java
+++ src/CalculFacture.java
@@ -4,6 +4,12 @@ public class CalculFacture {
     public static final char VEHICULE_PETIT = 'p';
     public static final char VEHICULE_INTERMEDIAIRE = 'i';
     public static final char VEHICULE_GRAND = 'g';
+    public static final float PRIX_HYBRIDE_PETIT = 55.75f;
+    public static final float PRIX_HYBRIDE_INTERMEDIAIRE = 60.25f;
+    public static final float PRIX_HYBRIDE_GRAND = 65.50f;
+    public static final float PRIX_ELECTRIQUE_PETIT = 45.50f;
+    public static final float PRIX_ELECTRIQUE_INTERMEDIAIRE = 50.50f;
+    public static final float PRIX_ELECTRIQUE_GRAND = 55.25f;
 
     public static int voituresDisponibles(char type, char grandeur, int nbLouesHP, int nbLouesHI, int nbLouesHG, int nbLouesEP, int nbLouesEI, int nbLouesEG) {
 
@@ -31,7 +37,32 @@ public class CalculFacture {
         return -1;
     }
 
+    public static float prixLocationTypeGrandeur(char type, char grandeur) {
+        if (type == VEHICULE_HYBRIDE) {
+            if (grandeur == VEHICULE_PETIT) {
+                return PRIX_HYBRIDE_PETIT;
+            } else if (grandeur == VEHICULE_INTERMEDIAIRE) {
+                return PRIX_HYBRIDE_INTERMEDIAIRE;
+            } else if (grandeur == VEHICULE_GRAND) {
+                return PRIX_HYBRIDE_GRAND;
+            }
+
+        } else {
+            if (type == VEHICULE_ELECTRIQUE) {
+                if (grandeur == VEHICULE_PETIT) {
+                    return PRIX_ELECTRIQUE_PETIT;
+                } else if (grandeur == VEHICULE_INTERMEDIAIRE) {
+                    return PRIX_ELECTRIQUE_INTERMEDIAIRE;
+                } else if (grandeur == VEHICULE_GRAND) {
+                    return PRIX_ELECTRIQUE_GRAND;
+                }
+            }
+        }
+        return -1;
+    }
 }
+
+
 
 
 
